94ca92e2b356cb4331b49738716881361a4cbba7
mohanraj19 <mohanrajms19061999@gmail.com>
2025-02-11 16:01:38 +0530

    level 6

--- demoapp/webapp/Component.js
+++ demoapp/webapp/Component.js
@@ -1,26 +1,24 @@
 sap.ui.define([
-    "sap/ui/core/UIComponent",
-    "demoapp/model/models"
-], (UIComponent, models) => {
+    "sap/ui/core/UIComponent"
+  
+], (UIComponent) => {
     "use strict";
 
     return UIComponent.extend("demoapp.Component", {
         metadata: {
-            manifest: "json",
-            interfaces: [
-                "sap.ui.core.IAsyncContentCreation"
-            ]
+            manifest: "json"
+           
         },
 
-        init() {
+        init: function() {
             // call the base component's init function
             UIComponent.prototype.init.apply(this, arguments);
 
             // set the device model
-            this.setModel(models.createDeviceModel(), "device");
+           // this.setModel(models.createDeviceModel(), "device");
 
             // enable routing
-            //this.getRouter().initialize();
+            this.getRouter().initialize();
         }
     });
 });
--- demoapp/webapp/controller/App.controller.js
+++ demoapp/webapp/controller/App.controller.js
@@ -1,24 +1,12 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], (Controller) => {
+  "demoapp/controller/BaseController"
+], (BaseController) => {
   "use strict";
 
-  return Controller.extend("demoapp.controller.App", {
+  return BaseController.extend("demoapp.controller.App", {
     onInit: function () {
-      this.reloadData();
-  },
-  reloadData: function () {
-      let that = this;
-      if (window.performance) {
-        console.info("window.performance works fine on this browser");
-      }
-      console.info(performance.navigation.type);
-      if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
-        //this.setManifest(1);
-       // this.setSideNavigation();
-      } else {
-        console.info("This page is not reloaded");
-      }
-    }
+     
+  }
+
   });
 });
