ed00bbeeb3ebf2329b884cc59c0c88aeb8a96bad
mohanraj19 <mohanrajms19061999@gmail.com>
2025-02-12 11:12:13 +0530

    level 8

---
+++ demoapp/webapp/controller/employee/overview/EmployeeOverview.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+	"demoapp/controller/BaseController"
+], function (BaseController) {
+	"use strict";
+	return BaseController.extend("demoapp.controller.employee.overview.EmployeeOverview", {
+	});
+});
