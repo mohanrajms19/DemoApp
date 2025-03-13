f6211ef001422ce6c9a70f9c9e6885c3df1f83db
mohanraj19 <mohanrajms19061999@gmail.com>
2025-03-13 13:33:44 +0530

    level 14

--- srv/src/main/java/customer/demoapp/employee/EmployeeController.java
+++ srv/src/main/java/customer/demoapp/employee/EmployeeController.java
@@ -1,8 +1,10 @@
 package customer.demoapp.employee;
 
-// import customer.demoapp.employee.Employee;
-// import customer.demoapp.employee.EmployeeService;
+ import customer.demoapp.employee.Employee;
+ import customer.demoapp.employee.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- srv/src/main/java/customer/demoapp/employee/EmployeeRepository.java
+++ srv/src/main/java/customer/demoapp/employee/EmployeeRepository.java
@@ -1,10 +1,11 @@
 package customer.demoapp.employee;
-
+import java.util.Optional;
 // import customer.demoapp.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
+public interface EmployeeRepository extends JpaRepository<Employee, Long> , JpaSpecificationExecutor<Employee>{
+    Optional<Employee> findById(Long id);
 }
